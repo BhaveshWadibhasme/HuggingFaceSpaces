bc1b7b29acf0e817f05f55f1ef2c3ce7063aee70
Bhavesh Wadibhasme <37530172+BhaveshWadibhasme@users.noreply.github.com>
2022-08-27 17:44:39 +0530

    updated code file and requirements file

---
+++ app.py
@@ -0,0 +1,17 @@
+import numpy
+import streamlit as st
+
+st.title("This is my first app")
+
+value1 = st.slider(
+     'Select a range of values for first argument',
+     0.0, 100.0)
+
+value2 = st.slider(
+     'Select a range of values for second argument',
+     0.0, 100.0)
+
+
+addition = value1 + value2
+
+st.write("The addition is",addition)
